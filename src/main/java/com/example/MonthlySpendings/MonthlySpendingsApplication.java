887fecbaab91a6fd5e31534dfc02c90150fa3ae7
jongpants <jong.lau99@gmail.com>
2021-10-24 23:43:45 +0300

    First commit

---
+++ src/main/java/com/example/MonthlySpendings/MonthlySpendingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonthlySpendings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlySpendingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlySpendingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MonthlySpendings/MonthlySpendingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonthlySpendings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlySpendingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
